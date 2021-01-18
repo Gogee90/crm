8555b13e8f7e72329f96c340cb185ed2f713a5b8
Gogee90 <dead69-not@yandex.ru>
2021-01-18 12:35:01 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://dead69not.pythonanywhere.com/api'
+axios.defaults.baseURL = 'https://dead69not.pythonanywhere.com/api'
 
 Vue.config.productionTip = false
 
